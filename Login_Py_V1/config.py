4c86d51d91775c73b3446d50055e65e42780c1a7
almeida-cma <93884913+almeida-cma@users.noreply.github.com>
2025-03-28 18:58:37 -0300

    Update config.py

--- Login_Py_V1/config.py
+++ Login_Py_V1/config.py
@@ -4,7 +4,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "chave_secreta_super_segura")
 
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USER = os.getenv("EMAIL_USER", "almeida.cma@gmail.com")
-EMAIL_PASS = os.getenv("EMAIL_PASS", "ufkt dfcs tzkq fspd")
+EMAIL_USER = os.getenv("EMAIL_USER", "mail@gmail.com")
+EMAIL_PASS = os.getenv("EMAIL_PASS", "senha app")
 
 DATABASE = "database.db"  # Agora definida corretamente
